43a650c673b2f5d025379c9ade3d7d1a79a3b3fb
Aditya Bhardwaj <adityabhardwajpcm@gmail.com>
2026-05-06 17:44:45 +0530

    andTwo16BitNumbers.asm

---
+++ andTwo16BitNumbers.asm
@@ -0,0 +1,22 @@
+; Write an 8086 assembly program to perform bitwise AND on two 16-bit numbers
+; and store the result
+
+; Name - ADITYA BHARDWAJ
+; Section - D2
+; Roll No - 07
+; Course – B TECH
+; Branch – CSE
+
+ORG 2000H
+
+MOV AX, 0000H        ; Clear AX
+MOV DS, AX           ; Initialize DS
+
+MOV AX, [1200H]      ; Load first 16-bit number into AX
+MOV BX, [1202H]      ; Load second 16-bit number into BX
+
+AND AX, BX           ; Bitwise AND: AX = AX AND BX
+
+MOV [1204H], AX      ; Store result
+
+HLT                  ; Stop execution
